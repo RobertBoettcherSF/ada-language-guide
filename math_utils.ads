d61e8edd58bc0bfa34c43005fa39acf72848688d
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:19:13 +0200

    Create math_utils.ads

---
+++ math_utils.ads
@@ -0,0 +1,6 @@
+-- File: math_utils.ads
+package Math_Utils is
+   -- Function declaration
+   function Add(X, Y: Integer) return Integer;
+   function Subtract(X, Y: Integer) return Integer;
+end Math_Utils;
