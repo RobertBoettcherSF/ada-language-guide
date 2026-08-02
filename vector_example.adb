bd802391e38398fd6ee949812b613a492dcbb1dc
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:21:53 +0200

    Create vector_example.adb

---
+++ vector_example.adb
@@ -0,0 +1,37 @@
+-- File: vector_example.adb
+with Ada.Text_IO;                 use Ada.Text_IO;
+with Ada.Containers.Vectors;
+
+procedure Vector_Example is
+   -- Define a vector of integers
+   package Integer_Vectors is new Ada.Containers.Vectors(
+      Index_Type => Natural,
+      Element_Type => Integer
+   );
+   use Integer_Vectors;
+   
+   V: Vector;
+begin
+   -- Append elements
+   V.Append(10);
+   V.Append(20);
+   V.Append(30);
+   
+   -- Insert at beginning
+   V.Prepend(5);
+   
+   -- Print all elements
+   Put_Line("Vector elements:");
+   for I in 1 .. Length(V) loop
+      Put_Line(Integer'Image(Element(V, I)));
+   end loop;
+   
+   -- Delete an element
+   V.Delete(2);  -- Delete the second element (20)
+   
+   -- Print after deletion
+   Put_Line("After deleting element 2:");
+   for I in 1 .. Length(V) loop
+      Put_Line(Integer'Image(Element(V, I)));
+   end loop;
+end Vector_Example;
