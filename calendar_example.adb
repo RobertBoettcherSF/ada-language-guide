ad5036e62929ee48879f37f9360f3547868307a6
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:30:36 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -8,3 +8,20 @@ procedure Calendar_Example is
    Year: Year_Number;
    Month: Month_Number;
    Day: Day_Number;
+   Seconds: Second_Number;
+begin  -- <-- MISSING IN YOUR FILE
+   -- Split time into components
+   Split(Now, Year, Month, Day, Seconds);
+
+   -- Print formatted date
+   Put_Line("Current date: " &
+            Year_Number'Image(Year) & "/" &
+            Month_Number'Image(Month) & "/" &
+            Day_Number'Image(Day));
+
+   -- Print formatted time
+   Put_Line("Current time: " &
+            Hour_Number'Image(Hour(Now)) & ":" &
+            Minute_Number'Image(Minute(Now)) & ":" &
+            Second_Number'Image(Seconds));
+end Calendar_Example;  -- <-- MISSING IN YOUR FILE
