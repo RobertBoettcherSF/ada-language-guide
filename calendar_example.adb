3d74c2e51c60f6e93baf6ec38c6dd31413d98405
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:50:58 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -5,14 +5,15 @@ with Ada.Calendar; use Ada.Calendar;  -- This makes Hour/Minute/Second visible
 procedure Calendar_Example is
    Now: constant Time := Clock;
 begin
-   -- Use fully qualified names or rely on "use Ada.Calendar"
+   -- Use fully qualified names OR rely on "use Ada.Calendar"
    Put_Line("Current date: " &
             Integer'Image(Integer(Year(Now))) & "/" &
             Integer'Image(Integer(Month(Now))) & "/" &
             Integer'Image(Integer(Day(Now))));
 
+   -- FIX: Use Ada.Calendar.Hour, etc. (or rely on "use Ada.Calendar")
    Put_Line("Current time: " &
-            Integer'Image(Integer(Hour(Now))) & ":" &   -- Now visible via "use Ada.Calendar"
+            Integer'Image(Integer(Hour(Now))) & ":" &  -- Now visible via "use Ada.Calendar"
             Integer'Image(Integer(Minute(Now))) & ":" &
             Integer'Image(Integer(Second(Now))));
 end Calendar_Example;
