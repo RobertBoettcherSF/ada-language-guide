0da79032f6fa1b7789b901cf0305d31ea1da2dae
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:33:26 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -5,23 +5,15 @@ with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
 
 procedure Calendar_Example is
    Now: Time := Clock;  -- Current time
-   Year: Year_Number;
-   Month: Month_Number;
-   Day: Day_Number;
-   Seconds: Second_Number;
-begin  -- <-- MISSING IN YOUR FILE
-   -- Split time into components
-   Split(Now, Year, Month, Day, Seconds);
-
-   -- Print formatted date
+begin
+   -- Print formatted date and time using individual functions
    Put_Line("Current date: " &
-            Year_Number'Image(Year) & "/" &
-            Month_Number'Image(Month) & "/" &
-            Day_Number'Image(Day));
+            Year_Number'Image(Year(Now)) & "/" &
+            Month_Number'Image(Month(Now)) & "/" &
+            Day_Number'Image(Day(Now)));
 
-   -- Print formatted time
    Put_Line("Current time: " &
             Hour_Number'Image(Hour(Now)) & ":" &
             Minute_Number'Image(Minute(Now)) & ":" &
-            Second_Number'Image(Seconds));
-end Calendar_Example;  -- <-- MISSING IN YOUR FILE
+            Second_Number'Image(Second(Now)));
+end Calendar_Example;
