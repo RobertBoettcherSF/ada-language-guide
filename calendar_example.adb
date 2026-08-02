194599c70a061289d493315a163df9606f22bb9c
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:36:58 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -1,19 +1,18 @@
 -- File: calendar_example.adb
-with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Calendar;           use Ada.Calendar;
-with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Calendar; use Ada.Calendar;  -- Only use Ada.Calendar (not Formatting)
 
 procedure Calendar_Example is
    Now: Time := Clock;  -- Current time
 begin
-   -- Print formatted date and time using individual functions
+   -- Use fully qualified names to avoid ambiguity
    Put_Line("Current date: " &
-            Year_Number'Image(Year(Now)) & "/" &
-            Month_Number'Image(Month(Now)) & "/" &
-            Day_Number'Image(Day(Now)));
+            Ada.Calendar.Year_Number'Image(Ada.Calendar.Year(Now)) & "/" &
+            Ada.Calendar.Month_Number'Image(Ada.Calendar.Month(Now)) & "/" &
+            Ada.Calendar.Day_Number'Image(Ada.Calendar.Day(Now)));
 
    Put_Line("Current time: " &
-            Hour_Number'Image(Hour(Now)) & ":" &
-            Minute_Number'Image(Minute(Now)) & ":" &
-            Second_Number'Image(Second(Now)));
+            Ada.Calendar.Hour_Number'Image(Ada.Calendar.Hour(Now)) & ":" &
+            Ada.Calendar.Minute_Number'Image(Ada.Calendar.Minute(Now)) & ":" &
+            Ada.Calendar.Second_Number'Image(Ada.Calendar.Second(Now)));
 end Calendar_Example;
