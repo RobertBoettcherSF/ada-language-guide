7fad5ca11adfd89ca84aa3dc876223025cce3cd9
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:58:04 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -1,31 +1,19 @@
 -- File: calendar_example.adb
-with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Calendar;           use Ada.Calendar;
-with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Calendar; use Ada.Calendar;
 
 procedure Calendar_Example is
    Now: Time := Clock;
-   Year: Year_Number;
-   Month: Month_Number;
-   Day: Day_Number;
-   Hour: Hour_Number;
-   Minute: Minute_Number;
-   Second: Second_Number;
-   Seconds: Day_Duration;  -- Required for Split
 begin
-   -- Split the time into components
-   Split(Now, Year, Month, Day, Seconds);
-
-   -- Extract hour, minute, second from Seconds (if needed)
-   -- For simplicity, just print date and total seconds
+   -- Print date components (Year/Month/Day are always available)
    Put_Line("Current date: " &
-            Year_Number'Image(Year) & "/" &
-            Month_Number'Image(Month) & "/" &
-            Day_Number'Image(Day));
+            Year(Now)'Image & "/" &
+            Month(Now)'Image & "/" &
+            Day(Now)'Image);
 
-   -- Print time using Hour, Minute, Second functions
+   -- Print time components (Hour/Minute/Second are available in Ada.Calendar)
    Put_Line("Current time: " &
-            Hour_Number'Image(Hour(Now)) & ":" &
-            Minute_Number'Image(Minute(Now)) & ":" &
-            Second_Number'Image(Second(Now)));
+            Hour(Now)'Image & ":" &   -- FIX: Use Hour(Now)'Image, NOT Hour_Number'Image(Hour(Now))
+            Minute(Now)'Image & ":" &
+            Second(Now)'Image);
 end Calendar_Example;
