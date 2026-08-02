8dbef0a23a42ee3661cef9badacb6a833fcf6b1a
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:41:06 +0200

    Update calendar_example.adb

--- calendar_example.adb
+++ calendar_example.adb
@@ -1,18 +1,18 @@
 -- File: calendar_example.adb
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Calendar; use Ada.Calendar;  -- Only use Ada.Calendar (not Formatting)
+with Ada.Calendar; use Ada.Calendar;
 
 procedure Calendar_Example is
    Now: Time := Clock;  -- Current time
 begin
-   -- Use fully qualified names to avoid ambiguity
+   -- Use Integer'Image for all components (no ambiguity)
    Put_Line("Current date: " &
-            Ada.Calendar.Year_Number'Image(Ada.Calendar.Year(Now)) & "/" &
-            Ada.Calendar.Month_Number'Image(Ada.Calendar.Month(Now)) & "/" &
-            Ada.Calendar.Day_Number'Image(Ada.Calendar.Day(Now)));
+            Integer'Image(Integer(Year(Now))) & "/" &
+            Integer'Image(Integer(Month(Now))) & "/" &
+            Integer'Image(Integer(Day(Now))));
 
    Put_Line("Current time: " &
-            Ada.Calendar.Hour_Number'Image(Ada.Calendar.Hour(Now)) & ":" &
-            Ada.Calendar.Minute_Number'Image(Ada.Calendar.Minute(Now)) & ":" &
-            Ada.Calendar.Second_Number'Image(Ada.Calendar.Second(Now)));
+            Integer'Image(Integer(Hour(Now))) & ":" &
+            Integer'Image(Integer(Minute(Now))) & ":" &
+            Integer'Image(Integer(Second(Now))));
 end Calendar_Example;
