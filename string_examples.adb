49e1dabb53213e0973f2f2916eae383f569dac75
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-02 10:47:37 +0200

    Update string_examples.adb

--- string_examples.adb
+++ string_examples.adb
@@ -2,24 +2,25 @@
 with Ada.Text_IO;            use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Fixed;     use Ada.Strings.Fixed;
+with Ada.Strings;           use Ada.Strings;  -- Add this line
 
 procedure String_Examples is
    -- Fixed-length string
    S1: String(1 .. 10) := "Hello     ";
-   
+
    -- Unbounded string
    S2: Unbounded_String := To_Unbounded_String("World");
-   
+
    -- Trimmed string
-   S3: String(1 .. 5) := Trim(S1, Both);
+   S3: String(1 .. 5) := Trim(S1, Both);  -- Now "Both" is visible
 begin
    -- Print fixed-length string
    Put_Line("Fixed-length string: '" & S1 & "'");
-   
+
    -- Append to unbounded string
-   S2 := S2 & "!" & " Ada";
+   S2 := S2 & "! Ada";
    Put_Line("Unbounded string: '" & To_String(S2) & "'");
-   
+
    -- Print trimmed string
    Put_Line("Trimmed string: '" & S3 & "'");
 end String_Examples;
